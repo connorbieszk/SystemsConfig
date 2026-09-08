f9d0ddda85bce11f15209219e27c244117e7a82b
Connor B. <98125183+connorbieszk@users.noreply.github.com>
2026-09-08 00:12:24 -0500

    Add: Comin

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     preservation.url = "github:nix-community/preservation";
+        comin = {
+      url = "github:nlewo/comin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs = inputs@{ self, nixpkgs, ... }: {
     # NOTE: 'nixos' is the default hostname
@@ -15,6 +19,18 @@
         ./hosts/astro/voyager/configuration.nix 
         ./hosts/astro/voyager/disko.nix
         ./hosts/astro/voyager/preservation.nix
+
+        inputs.comin.nixosModules.comin
+          ({
+            services.comin = {
+              enable = true;
+              remotes = [{
+                name = "origin";
+                url = "https://github.com/connorbieszk/SystemsConfig.git";
+                branches.main.name = "main";
+              }];
+            };
+          })
         ];
     };
   };
