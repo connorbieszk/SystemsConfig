7e2e80314a73730ec287d486ca2bad1df5bfbdc4
Connor B. <bieszcon29@stu.cedarburg.k12.wi.us>
2026-09-07 20:13:02 -0600

    Fix: forget there is minimal space on device when first installing
    Temporarily disabled non-essential packages

--- configuration.nix
+++ configuration.nix
@@ -21,8 +21,8 @@
     keyMap = "us";
   };
 
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+#  services.displayManager.gdm.enable = true;
+#  services.desktopManager.gnome.enable = true;
   
 
   services.pipewire = {
@@ -47,10 +47,10 @@
 
   environment.systemPackages = with pkgs; [
     micro
-    curl
+    #curl
     git
-    wget
-    vscode
+    #wget
+    #vscode
   ];
 
   zramSwap = {
@@ -65,13 +65,13 @@
     "vm.watermark_boost_factor" = 0;
   };
 
-  nixpkgs.config.allowUnfree = true;
+  #nixpkgs.config.allowUnfree = true;
 
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  #programs.mtr.enable = true;
+  #programs.gnupg.agent = {
+  #  enable = true;
+  #  enableSSHSupport = true;
+  #};
 
   system.stateVersion = "26.11";
 }
