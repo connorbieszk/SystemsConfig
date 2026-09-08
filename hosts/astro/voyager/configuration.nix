053919a5e03b4df29ba275f796528262eeff1943
Connor B. <98125183+connorbieszk@users.noreply.github.com>
2026-09-08 00:05:27 -0500

    Make gnome quieter

--- hosts/astro/voyager/configuration.nix
+++ hosts/astro/voyager/configuration.nix
@@ -24,6 +24,14 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
   
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+    gnome-shell-extensions
+    ];
 
   services.pipewire = {
     enable = true;
@@ -49,6 +57,7 @@
   environment.systemPackages = with pkgs; [
     micro
     curl
+    kitty
     git
     wget
     vscode
